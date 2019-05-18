3c5a35b21deab7e3d3f4f274f7bba56615d412f2
forewing <jujianai@hotmail.com>
2019-05-18 10:23:47 +0800

    add admin

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from app.models import Room, User
+
 # Register your models here.
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'roomid', 'status')
+    list_filter = ['roomid']
+    search_fields = ['roomid']
+
+
+admin.site.register(Room, RoomAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'room', 'status')
+    list_filter = ['name', 'room']
+    search_fields = ['name']
+
+
+admin.site.register(User, UserAdmin)
