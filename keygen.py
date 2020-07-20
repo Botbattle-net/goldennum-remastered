579797e96e2f2d2b97b9ef4b1bcb6893b79df201
forewing <jujianai@hotmail.com>
2020-07-20 15:24:27 +0800

    update configs

--- keygen.py
+++ keygen.py
@@ -4,5 +4,5 @@ import string
 key = ''.join([random.SystemRandom().choice(string.ascii_lowercase)
                for i in range(0, 50)])
 
-with open('web/conf/secret_key.py', 'w') as File:
+with open('web/web/secret_key.py', 'w') as File:
     File.write("secret_key = '" + key + "'")
--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --clear --no-input
 cp ./favicon.ico /var/www/storage/goldennum/
-gunicorn -b 0.0.0.0:8080 -w4 web.wsgi
+gunicorn -b 0.0.0.0:8080 --workers=4 --threads=2 web.wsgi
